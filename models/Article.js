d8e985b980c1cf9abe43cde0f25bd5fadb5c327e
jieping <2495413242@qq.com>
2022-08-31 12:23:38 +0800

    新增了article的CRUD

--- models/Article.js
+++ models/Article.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema
 
 // 实例化模板
 const ArticleSchema = new Schema({
-  
   title: {
     type: String,
     required: true,
@@ -11,32 +10,48 @@ const ArticleSchema = new Schema({
   // 发布日期
   pubDate: {
     type: Date,
-    required:true,
+    default: Date.now(),
+    // required:true,
   },
   // 修改日期
-  modiDate:{
-    type:Date,
-    default: Date.now
+  modiDate: {
+    type: Date,
+    default: Date.now(),
   },
   // 文章内容
-  mainContent:{
-    type:String,
-    default:''
+  mainContent: {
+    type: String,
+    default: '',
   },
   // 文章摘要
-  abstract:{
-    type:String,
-    default:''
+  abstract: {
+    type: String,
+    default: '',
   },
   // 所属分类
-  classify:{
-    type:String,
-    default:''
+  classify: {
+    type: String,
+    default: '',
   },
   // 关键词
-  keywords:{
-    type:Array,
-    default:[]
+  keywords: {
+    type: Array,
+    default: [],
+  },
+  // 点赞数
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  // 评论数
+  comments: {
+    type: Number,
+    default: 0,
+  },
+  // 浏览数
+  views: {
+    type: Number,
+    default: 0,
   },
   isActive: {
     type: Boolean,
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router')
+const { update } = require('../../models/Article')
 const router = new Router()
 // 引入modle
 const Article = require('../../models/Article')
@@ -8,10 +9,73 @@ const Article = require('../../models/Article')
  * @description 测试接口
  * @access      接口公开
  */
-
- router.get('/', async (ctx) => {
+router.get('/test', async (ctx) => {
   ctx.status = 200
   ctx.body = { msg: 'articles ok...' }
+})
+
+/**
+ * @route POST api/articles/
+ * @description 新增一篇文章
+ * @access      接口公开
+ */
+router.post('/', async (ctx) => {
+  const newArticle = new Article()
+  const body = ctx.request.body
+  Object.keys(body).forEach((key) => {
+    newArticle[key] = body[key]
+  })
+  await newArticle.save()
+  ctx.status = 201
+  ctx.body = { success: true }
+})
+
+/**
+ * @route GET api/articles/
+ * @description 获取所有文章
+ * @access      接口公开
+ */
+router.get('/', async (ctx) => {
+  const query = { isActive: true }
+  const findResult = await Article.find(query)
+  ctx.status = 200
+  ctx.body = { status: ctx.status, data: { findResult } }
+})
+
+/**
+ * @route GET api/articles/:id
+ * @description 获取指定id的文章
+ * @access      接口公开
+ */
+router.get('/:id', async (ctx) => {
+  const { id } = ctx.params
+  const findResult = await Article.findById(id)
+  ctx.body = { data: { findResult } }
+})
+
+/**
+ * @route PATCH api/articles/:id
+ * @description 部分修改指定id的文章
+ * @access      接口公开
+ */
+router.patch('/:id', async (ctx) => {
+  const { id } = ctx.params
+  const update = ctx.request.body
+  update.modiDate = Date.now()
+  await Article.findByIdAndUpdate(id, update)
+  ctx.status = 200
+  ctx.body = { success: true }
+})
+
+/**
+ * @route DELETE api/articles/:id
+ * @description 删除指定id的文章
+ * @access      接口公开
+ */
+router.delete('/:id', async (ctx) => {
+  const { id } = ctx.params
+  await Article.findByIdAndDelete(id)
+  ctx.status = 204
 })
 
 module.exports = router.routes()
